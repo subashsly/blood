6b2502c4d07fd2d5dccccbcc9989504748ae856c
Subash Shahi <subashsly33@gmail.com>
2018-01-11 20:44:54 +0545

    pg

--- Gemfile
+++ Gemfile
@@ -67,7 +67,7 @@ end
 
 group :production do
   gem 'rails_12factor'
-  gem 'pg'
+  gem 'pg', '~> 0.20'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :user_queries
   resources :blood_requests
   devise_for :users
+  get "about", to: "static_pages#about"
+  get "contact", to: "static_pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#home"
 end
