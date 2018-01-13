1e90b7a290f3c506ab6db5dce961862b7c77cb12
Subash Shahi <subashsly33@gmail.com>
2018-01-13 20:06:24 +0545

    sf

--- db/migrate/20180113045620_remove_index_users_on_unlock_token_from_user.rb
+++ db/migrate/20180113045620_remove_index_users_on_unlock_token_from_user.rb
@@ -1,5 +1,5 @@
-class RemoveIndexUsersOnUnlockTokenFromUser < ActiveRecord::Migration[5.1]
-  def change
-  	remove_index(:users, :name => "index_users_on_unlock_token")
-  end
-end
+# class RemoveIndexUsersOnUnlockTokenFromUser < ActiveRecord::Migration[5.1]
+#   def change
+#   	remove_index(:users, :name => "index_users_on_unlock_token")
+#   end
+# end
