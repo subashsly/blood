662433960c6136a89c3ae3024fe9abdca94fdd4b
Subash Shahi <subashsly33@gmail.com>
2018-01-14 14:00:40 +0545

    style

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 	enum gender: { male: 0, female: 1 } 
 	enum blood_group: { "O-ve" => 0,"O+ve" => 1,"A+ve" => 2,"A-ve" => 3,"B+ve" => 4,"B-ve" => 5,"AB+ve" => 6,"AB-ve" => 7, } 
 	validates :email, :first_name, :last_name, :phone_no, :gender, :address, :city,  :blood_group, :weight, presence: true
+  	
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
